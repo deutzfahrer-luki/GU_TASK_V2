81443b4beb90b24bbcf8fb1725cc113cf84b87de
deutzfahrer-luki <traktor.koeppl@gmail.com>
2024-12-15 09:48:45 +0100

    add Relative Due to main logik and add to relativeDue a getter for string datatyp (implement RelativeDue in tasktablemodel.h was on the last commit)

--- model/RelativeDue.h
+++ model/RelativeDue.h
@@ -1,6 +1,8 @@
 #ifndef RELATIVEDUE_H
 #define RELATIVEDUE_H
 
+#include <string>
+
 enum class RelativeDue
 {
     Irrelevant,
@@ -11,5 +13,19 @@ enum class RelativeDue
     ThisMonth,
     Later
 };
+
+// Helper-Funktion zur Umwandlung von RelativeDue in einen String
+inline std::string relativeDueToString(RelativeDue state) {
+    switch (state) {
+    case RelativeDue::Irrelevant: return "Irrelevant";
+    case RelativeDue::Overdue:    return "Overdue";
+    case RelativeDue::Today:      return "Today";
+    case RelativeDue::Tomorrow:   return "Tomorrow";
+    case RelativeDue::ThisWeek:   return "ThisWeek";
+    case RelativeDue::ThisMonth:  return "ThisMonth";
+    case RelativeDue::Later:      return "Later";
+    default:                      return "Unknown";
+    }
+}
 
 #endif // RELATIVEDUE_H
--- model/task.h
+++ model/task.h
@@ -5,23 +5,23 @@
 #include <iostream>
 #include "util/date.h"
 #include "model/user.h"
+#include "model/RelativeDue.h"
 
 class Task
 {
 public:
-    Task(int id, const std::string& description, const Date& due, const User& assignee, const std::string& state)
+    Task(int id, const std::string& description, const Date& due, const User& assignee, const RelativeDue& state)
         : id(id), description(description), due(due), assignee(assignee), state(state) {}
 
     void setDone() {
-        if (state != "done") {
-            state = "done";
-        }
+
     }
 
     int getId() const { return id; }
     std::string getDescription() const { return description; }
     std::string getAssignee() const { return assignee.getFullName(); }
-    std::string getState() const { return state; }
+    RelativeDue getStateRelative() const { return state; }
+    std::string getStateByString() const { return relativeDueToString(state); }
     std::string getDue() const { return static_cast<std::string>(due); }
 
 private:
@@ -29,7 +29,7 @@ private:
     int id;
     std::string description;
     User assignee;
-    std::string state;
+    RelativeDue state;
 };
 
 #endif // TASK_H

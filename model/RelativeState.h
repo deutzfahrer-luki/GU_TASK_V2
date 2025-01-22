910522c6dc330efe36eea227f164eb7e701dadd9
deutzfahrer-luki <traktor.koeppl@gmail.com>
2025-01-22 21:57:33 +0100

    update the RelativeState and task

--- model/RelativeState.h
+++ model/RelativeState.h
@@ -1,10 +1,28 @@
 #ifndef RELATIVESTATE_H
 #define RELATIVESTATE_H
 
-enum class RelativeState
-{
+#include <string>
+#include <stdexcept>
+
+enum class RelativeState {
     Started,
     Finished
 };
+
+inline std::string relativeStateToString(RelativeState state) {
+    switch (state) {
+    case RelativeState::Started: return "Started";
+    case RelativeState::Finished: return "Finished";
+    default: return "Unknown";
+    }
+}
+
+inline RelativeState relativeStateFromString(const std::string& state) {
+    if (state == "Started") return RelativeState::Started;
+    if (state == "Finished") return RelativeState::Finished;
+
+    throw std::invalid_argument("Unrecognized RelativeState string: " + state);
+}
+
 
 #endif // RELATIVESTATE_H
--- model/task.h
+++ model/task.h
@@ -6,22 +6,19 @@
 #include "util/date.h"
 #include "model/user.h"
 #include "model/RelativeDue.h"
+#include "model/RelativeState.h"
 
 class Task
 {
 public:
-    Task(int id, const std::string& description, const Date& due, const User& assignee, const RelativeDue& state)
+    Task(int id, const std::string& description, const Date& due, const User& assignee, const RelativeState& state)
         : id(id), description(description), due(due), assignee(assignee), state(state) {}
-
-    void setDone() {
-
-    }
 
     int getId() const { return id; }
     std::string getDescription() const { return description; }
     std::string getAssignee() const { return assignee.getFullName(); }
-    RelativeDue getStateRelative() const { return state; }
-    std::string getStateByString() const { return relativeDueToString(state); }
+    RelativeState getStateRelative() const { return state; }
+    std::string getStateByString() const { return relativeStateToString(state); }
     std::string getDue() const { return static_cast<std::string>(due); }
 
 private:
@@ -29,7 +26,7 @@ private:
     int id;
     std::string description;
     User assignee;
-    RelativeDue state;
+    RelativeState state;
 };
 
 #endif // TASK_H
